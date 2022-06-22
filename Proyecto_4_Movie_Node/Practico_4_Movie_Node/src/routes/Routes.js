0c4013b318255ff4426beefca9e0c3d885578c32
eduardoportillo <eportillovirreira@gmail.com>
2022-06-22 18:53:31 -0400

    corriendo django practico 3

--- Proyecto_4_Movie_Node/Practico_4_Movie_Node/src/routes/Routes.js
+++ Proyecto_4_Movie_Node/Practico_4_Movie_Node/src/routes/Routes.js
@@ -1,16 +1,19 @@
 const express = require("express");
 const router = express.Router();
 
+//Middlewares
+const auth = require('../middlewares/auth');
+
 //Controllers
 const movieController = require("../controllers/MovieController");
 const AuthController = require("../controllers/Authcontroller");
 
-//Home
-router.get("/", movieController.getAll)
 
 //Auth
-router.post("/login", AuthController.singIn)
-router.post("/register", AuthController.singUp)
+router.post("/api/token/", AuthController.singIn)
+router.post("/entidades/signup/", AuthController.singUp)
 
+//Movie
+router.get("/entidades/movie/", auth, movieController.getAll)
 
 module.exports = router;
